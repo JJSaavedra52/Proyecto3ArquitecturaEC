c18bb0826aaccaef33bd15f37cbf77bed4dd9836
Santiago <sanct1318@gmail.com>
2023-11-04 11:20:36 -0500

    Serializacion finalizada
    Se testeo la logica del codigo y se hizo la serializacion estandar. El codigo se comporta como deberia, falta la serializacion bluetooth.

--- proyecto3.ino
+++ proyecto3.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 
 //Defines comunicación serial
 #define RxPin 10
@@ -16,15 +16,16 @@
 //Variables globales
 char contador = 0;
 const unsigned char nivel[] = {0, 13, 26, 40, 53, 67, 80, 93, 107, 120, 134, 147, 161, 174, 187, 201, 214, 228, 241, 255};
-char button;
-SoftwareSerial bluetooth = SoftwareSerial(RxPin, TxPin);
+bool estadoMotor = true; 
+char accion;
+//SoftwareSerial bluetooth = SoftwareSerial(RxPin, TxPin);
 
 void setup(){ 
   //Configuración comunicación serial
   pinMode(RxPin, INPUT);
   pinMode(TxPin, OUTPUT);
   Serial.begin(9600, SERIAL_8N1);
-  bluetooth.begin(9600);
+  //bluetooth.begin(9600);
 
   //Configuración de salida e inicialización para diodos
   pinMode(D1, OUTPUT);
@@ -41,45 +42,54 @@ void setup(){
   analogWrite(MOTOR, nivel[contador]);
 }
 
+
 void loop() {
-  if(contador == 0){
-    titilar(D1,2);
+  if(Serial.available()>0){
+	accion = Serial.read();
+  }
+  
+  if(estadoMotor == false || contador == 0){
+    titilar(D1,1);
   }else{
     digitalWrite(D1, LOW);
   }
 
+  //if (Serial.available()) button = Serial.readStringUntil(",").charAt(0);
+  //else if (bluetooth.available()) button = bluetooth.readStringUntil(",").charAt(0);
+  //else return;
+	
+  //Serial.write(button);
 
-  if (Serial.available()) button = Serial.readStringUntil(",").charAt(0);
-  else if (bluetooth.available()) button = bluetooth.readStringUntil(",").charAt(0);
-  else return;
-
-  Serial.write(button);
-
-  switch (button) {
+  switch (accion) {
     case 'P':
       parada();
+      accion = 0;
       break;
     case 'A':
       arranque();
+      accion = 0;
       break;
     case 'I':
       incremento();
+      accion = 0;
       break;
     case 'R':
       decremento();
+      accion = 0;
       break;
     case 'C':
       ciclo();
+      accion = 0;
       break;
-    case 'O':
-      oprimido();
+    default:
+      accion = 0;
       break;
   }
 }
 
 //Función de titilar, recibe el pin del diodo y la veces que lo hará
 void titilar(char diodo, char n) {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n * 2; i++) {
     delay(250);
     digitalWrite(diodo, !digitalRead(diodo)); 
   }
@@ -88,46 +98,55 @@ void titilar(char diodo, char n) {
 //Función de parada
 void parada() {
   analogWrite(MOTOR, nivel[0]);
+  estadoMotor = false;
 }
 
 //Función de arranque
 void arranque() {
   analogWrite(MOTOR, nivel[contador]);
+  estadoMotor = true;
 }
 
+//Función de incremento de velocidad
 void incremento() {
-  titilar(D2,4);
-  contador++;
-  if(contador > 19){
-    contador = 19;
+  if(estadoMotor == true){
+  	titilar(D2,2);
+  	contador++;
+
+  	if(contador > 19){
+    	contador = 19;
+  	}
+  	analogWrite(MOTOR, nivel[contador]);
   }
-  analogWrite(MOTOR, nivel[contador]);
 }
 
+//Función de decremento de velocidad
 void decremento() {
-  titilar(D4,4);
-  contador--;
+  if(estadoMotor == true){
+    titilar(D4,2);
+    contador--;
+
   if(contador < 0){
     contador = 0;
   }
   analogWrite(MOTOR, nivel[contador]);
+  }
 }
 
 void ciclo() {
-  if(MOTOR == 0) {
-    for(char i = 0; i < 19; i++) {
-      titilar(D2, 6);
+  if(estadoMotor == false || contador == 0) {
+    for(char i = 0; i <= 19; i++) {
+      titilar(D2, 3);
       analogWrite(MOTOR, nivel[i]);
     }
-  }
-  titilar(D3, 60);
-  for(char i = 19; i > 0; i--) {
-      titilar(D4, 8);
+    
+    titilar(D3,30);
+    
+    for(char i = 19; i >= 0; i--){
+      titilar(D4,4);
       analogWrite(MOTOR, nivel[i]);
     }
-  contador = 0;
-}
-
-void oprimido(){
-  
+    contador = 0;
+    estadoMotor = true;
+  	}
 }
